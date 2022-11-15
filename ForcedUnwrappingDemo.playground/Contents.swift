d2dbd2e2d23d36fbaaae69bd7fdfc98e4f8426aa
Davide Troise <davide@troise.net>
2022-11-15 17:16:50 +0100

    Complete code

--- ForcedUnwrappingDemo.playground/Contents.swift
+++ ForcedUnwrappingDemo.playground/Contents.swift
@@ -1,3 +1,28 @@
-import UIKit
+var greeting: String?
 
-var greeting = "Hello, playground"
+//print(greeting!)
+
+//greeting = "Hello, world!"
+//
+//print(greeting!)
+
+let hello = greeting?.count
+
+if let greeting {
+    print(greeting.count)
+} else {
+    print("greeting is nil")
+}
+
+print(greeting?.count ?? "nothing to count")
+
+func checkGreeting(_ greeting: String? = nil) {
+    guard let greeting else {
+        print("no greeting")
+        return
+    }
+    
+    print(greeting)
+}
+
+checkGreeting()
